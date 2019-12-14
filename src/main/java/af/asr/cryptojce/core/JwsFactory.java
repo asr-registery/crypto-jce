d69570b7147f21157011d4cd1a988dc44cf1ad13
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-14 09:23:49 +0430

    MAC, Crypto, Validation and Codec Utils

--- src/main/java/af/asr/cryptojce/core/JwsFactory.java
+++ src/main/java/af/asr/cryptojce/core/JwsFactory.java
@@ -7,9 +7,7 @@ import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 
 public class JwsFactory {
-
     public JwsSpec<String, String , X509Certificate,PrivateKey> getJWS(){
         return new JWSValidation();
     }
-
 }
